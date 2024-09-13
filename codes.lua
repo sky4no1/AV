952949e343a6980f2614b4191ed15b4b65734df4
sky4no1 <105664118+sky4no1@users.noreply.github.com>
2024-09-13 19:43:40 +0300

    Create codes.lua

---
+++ codes.lua
@@ -0,0 +1,15 @@
+return {
+    "AV500KLIKES",
+    "400KLIKES",
+    "300KLIKES",
+    "200KLIKES",
+    "100KLIKES",
+    "10KLIKES",
+    "AV50MIL",
+    "25MVISITS",
+    "10MVISITS",
+    "RELEASE",
+    "DELAY",
+    "70MVISITS",
+    "600KLIKES"
+}
